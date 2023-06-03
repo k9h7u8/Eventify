5d5fc861d31120dade5d5ae0d6d7a2d305e3fdff
Khushi Aggarwal <khushiaggarwal1302@gmail.com>
2023-06-03 12:11:03 +0530

    email sended

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -3,27 +3,27 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendMail = (email, name, desc, date, time, venue) => {
-    const msg = {
-        to: email,
-        from: 'khushiaggarwal1302@gmail.com', // Use the email address or domain you verified above
-        subject: 'Registered Successfully',
-        text: `Event Name: ${name},
+        const msg = {
+                to: email,
+                from: process.env.MY_EMAIL, // Use the email address or domain you verified above
+                subject: 'Congratulations you are successfully registered for the event 🎉',
+                text: `Event Name: ${name},
                 Description: ${desc},
                 Date: ${date},
                 Time: ${time},
                 Venue: ${venue}`,
-        html: `<strong> Event Name: ${name}<br>
+                html: `<strong> Event Name: ${name}<br>
                         Description: ${desc}<br>
                         Date: ${date}<br>
                         Time: ${time}<br>
                         Venue: ${venue}
                 </strong>`,
-    };
+        };
 
-    sgMail
-        .send(msg)
-        .then(response => console.log("Email sent successfully"))
-        .catch(error => console.log(error.message));
+        sgMail
+                .send(msg)
+                .then(response => console.log("Email sent successfully"))
+                .catch(error => console.log(error.message));
 
 }
 
